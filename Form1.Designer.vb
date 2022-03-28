f1683e99052ce5eca10e9993c538868c9dccc3c7
MuhammadFaisal <faisalichal555@gmail.com>
2022-03-28 20:50:08 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Textproductid = New System.Windows.Forms.TextBox()
+        Me.Textitemname = New System.Windows.Forms.TextBox()
+        Me.Textdesign = New System.Windows.Forms.TextBox()
+        Me.Combocolor = New System.Windows.Forms.ComboBox()
+        Me.DateTimePickerinsert = New System.Windows.Forms.DateTimePicker()
+        Me.RadioAllowed = New System.Windows.Forms.RadioButton()
+        Me.Radionotallowed = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(75, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(280, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CRUD Tutorial Form"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(75, 115)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Product ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(75, 162)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 28)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Item Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.Location = New System.Drawing.Point(75, 209)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 28)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Design"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label5.Location = New System.Drawing.Point(75, 259)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 28)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Color"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label6.Location = New System.Drawing.Point(75, 304)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(106, 28)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Item Date"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label7.Location = New System.Drawing.Point(75, 349)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(151, 28)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Warranty Date"
+        '
+        'Textproductid
+        '
+        Me.Textproductid.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Textproductid.Location = New System.Drawing.Point(230, 115)
+        Me.Textproductid.Name = "Textproductid"
+        Me.Textproductid.Size = New System.Drawing.Size(471, 30)
+        Me.Textproductid.TabIndex = 7
+        '
+        'Textitemname
+        '
+        Me.Textitemname.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Textitemname.Location = New System.Drawing.Point(230, 164)
+        Me.Textitemname.Name = "Textitemname"
+        Me.Textitemname.Size = New System.Drawing.Size(471, 30)
+        Me.Textitemname.TabIndex = 8
+        '
+        'Textdesign
+        '
+        Me.Textdesign.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Textdesign.Location = New System.Drawing.Point(230, 211)
+        Me.Textdesign.Name = "Textdesign"
+        Me.Textdesign.Size = New System.Drawing.Size(471, 30)
+        Me.Textdesign.TabIndex = 9
+        '
+        'Combocolor
+        '
+        Me.Combocolor.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Combocolor.FormattingEnabled = True
+        Me.Combocolor.Items.AddRange(New Object() {"Black", "White", "Blue", "Golden"})
+        Me.Combocolor.Location = New System.Drawing.Point(230, 259)
+        Me.Combocolor.Name = "Combocolor"
+        Me.Combocolor.Size = New System.Drawing.Size(471, 31)
+        Me.Combocolor.TabIndex = 10
+        '
+        'DateTimePickerinsert
+        '
+        Me.DateTimePickerinsert.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DateTimePickerinsert.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePickerinsert.Location = New System.Drawing.Point(230, 306)
+        Me.DateTimePickerinsert.Name = "DateTimePickerinsert"
+        Me.DateTimePickerinsert.Size = New System.Drawing.Size(471, 30)
+        Me.DateTimePickerinsert.TabIndex = 11
+        '
+        'RadioAllowed
+        '
+        Me.RadioAllowed.AutoSize = True
+        Me.RadioAllowed.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RadioAllowed.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.RadioAllowed.Location = New System.Drawing.Point(230, 354)
+        Me.RadioAllowed.Name = "RadioAllowed"
+        Me.RadioAllowed.Size = New System.Drawing.Size(91, 27)
+        Me.RadioAllowed.TabIndex = 12
+        Me.RadioAllowed.TabStop = True
+        Me.RadioAllowed.Text = "Allowed"
+        Me.RadioAllowed.UseVisualStyleBackColor = True
+        '
+        'Radionotallowed
+        '
+        Me.Radionotallowed.AutoSize = True
+        Me.Radionotallowed.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Radionotallowed.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Radionotallowed.Location = New System.Drawing.Point(327, 354)
+        Me.Radionotallowed.Name = "Radionotallowed"
+        Me.Radionotallowed.Size = New System.Drawing.Size(125, 27)
+        Me.Radionotallowed.TabIndex = 13
+        Me.Radionotallowed.TabStop = True
+        Me.Radionotallowed.Text = "Not Allowed"
+        Me.Radionotallowed.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Purple
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(230, 403)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 37)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Insert"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 446)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(689, 212)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Purple
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button2.Location = New System.Drawing.Point(337, 403)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(101, 37)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Purple
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button3.Location = New System.Drawing.Point(444, 403)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(101, 37)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Purple
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button4.Location = New System.Drawing.Point(551, 403)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(101, 37)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "Search"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(713, 670)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Radionotallowed)
+        Me.Controls.Add(Me.RadioAllowed)
+        Me.Controls.Add(Me.DateTimePickerinsert)
+        Me.Controls.Add(Me.Combocolor)
+        Me.Controls.Add(Me.Textdesign)
+        Me.Controls.Add(Me.Textitemname)
+        Me.Controls.Add(Me.Textproductid)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = " "
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Textproductid As TextBox
+    Friend WithEvents Textitemname As TextBox
+    Friend WithEvents Textdesign As TextBox
+    Friend WithEvents Combocolor As ComboBox
+    Friend WithEvents DateTimePickerinsert As DateTimePicker
+    Friend WithEvents RadioAllowed As RadioButton
+    Friend WithEvents Radionotallowed As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    ' Menaruh koneksi ke SQL di area publik agar bisa diakses dimanapun
+    Dim con As New SqlConnection("Data Source=MF;Initial Catalog=ProgrammingDB;Persist Security Info=True;User ID=faisallocal;Password=faisallocal") ' String di dalam diambil dari properti koneksi
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataInGrid() ' Ditaro disini agar ketika Form nya ngeload, dia otomatis load data
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Mendeklarasikan variabel. Textproductid Diambil date properti name alias di Designer
+        Dim pid As Integer = Textproductid.Text
+        Dim iname As String = Textitemname.Text
+        Dim design As String = Textdesign.Text
+        Dim color As String = Combocolor.Text
+        Dim insertdate As String = DateTimePickerinsert.Text
+        ' Membuat logika if-else untuk RadioButton
+        Dim wtype As String = ""
+        If RadioAllowed.Checked = True Then
+            wtype = "Allowed"
+        Else
+            wtype = "Not Allowed"
+        End If
+
+        'Melakukan koneksi ke SQL Server, memanggil class library dulu
+        Dim con As New SqlConnection("Data Source=MF;Initial Catalog=ProgrammingDB;Persist Security Info=True;User ID=faisallocal;Password=faisallocal") ' String di dalam diambil dari properti koneksi
+        con.Open()
+        Dim command As New SqlCommand("Insert into Product_Setup_Tab values('" & pid & "', '" & iname & "', '" & design & "', '" & color & "', '" & insertdate & "', '" & wtype & "')", con) ' ('" & wtype & "') untuk membungkus variabel
+        command.ExecuteNonQuery() ' Untuk melakukan operasi Insert, Update & Delete
+        con.Close() ' Habis koneksi dibuka, harus ditutup
+        MessageBox.Show("Successfully Inserted") ' Memberi pemberitahuan
+
+        LoadDataInGrid() ' Untuk load data setelah di button click
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Mendeklarasikan variabel. Textproductid Diambil date properti name alias di Designer
+        Dim pid As Integer = Textproductid.Text
+        Dim iname As String = Textitemname.Text
+        Dim design As String = Textdesign.Text
+        Dim color As String = Combocolor.Text
+        Dim insertdate As String = DateTimePickerinsert.Text
+        ' Membuat logika if-else untuk RadioButton
+        Dim wtype As String = ""
+        If RadioAllowed.Checked = True Then
+            wtype = "Allowed"
+        Else
+            wtype = "Not Allowed"
+        End If
+
+        'Melakukan koneksi ke SQL Server, memanggil class library dulu
+        Dim con As New SqlConnection("Data Source=MF;Initial Catalog=ProgrammingDB;Persist Security Info=True;User ID=faisallocal;Password=faisallocal") ' String di dalam diambil dari properti koneksi
+        con.Open()
+        Dim command As New SqlCommand("update Product_Setup_Tab set ItemName = '" & iname & "', Design = '" & design & "', Color = '" & color & "', ItemDate = '" & insertdate & "', WarrantyType = '" & wtype & "' where Product_ID = '" & pid & "' ", con) ' ('" & wtype & "') untuk membungkus variabel
+        command.ExecuteNonQuery() ' Untuk melakukan operasi Insert, Update & Delete
+        con.Close() ' Habis koneksi dibuka, harus ditutup
+        MessageBox.Show("Successfully Updated") ' Memberi pemberitahuan
+
+        LoadDataInGrid() ' Untuk load data setelah di button click
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Menambahkan konfirmasi hapus. Jika ya maka hapus, jika tidak jangan lakukan apapun
+        If MessageBox.Show("Are you sure to delete?", "Delete Document", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Dim pid As Integer = Textproductid.Text
+            con.Open()
+            Dim command As New SqlCommand("delete from Product_Setup_Tab where Product_ID = '" & pid & "' ", con)
+            command.ExecuteNonQuery()
+            con.Close()
+            MessageBox.Show("Successfully Deleted")
+
+            LoadDataInGrid()
+
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Menambahkan metode untuk me-load data ke UI datagridview
+    ''' </summary>
+    Private Sub LoadDataInGrid() ' Sub apa?
+        Dim command As New SqlCommand("select * from Product_Setup_Tab", con)
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        DataGridView1.DataSource = dt ' DataGridView1 dari nama UI nya
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim pid As Integer = Textproductid.Text
+
+        Dim command As New SqlCommand("select * from Product_Setup_Tab where Product_ID = '" & pid & "' ", con)
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        DataGridView1.DataSource = dt ' DataGridView1 dari nama UI nya
+
+    End Sub
+End Class
